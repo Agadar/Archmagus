ef37bcd32283ea84cc7ba233b28cccfa7ee7d42e
Agadar <martin_donkersloot@hotmail.com>
2016-02-21 18:49:19 +0100

    Mana bar no longer conflicts with air bar.

--- java/com/agadar/archmagus/misc/GuiManaBar.java
+++ java/com/agadar/archmagus/misc/GuiManaBar.java
@@ -6,8 +6,10 @@ import com.agadar.archmagus.potion.ModPotions;
 
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -32,41 +34,74 @@ public class GuiManaBar extends Gui
 		if (event.isCancelable() || event.type != ElementType.FOOD)
 			return;
 
-		mc.mcProfiler.startSection("mana");
-		ManaProperties props = ManaProperties.get(this.mc.thePlayer);
+		// Air bar
 		
-		if (props == null || props.getMaxMana() == 0)
-			return;
-		
-		mc.getTextureManager().bindTexture(modIcons);
-		GL11.glEnable(GL11.GL_BLEND);
-		int left = event.resolution.getScaledWidth() / 2 + 91;
-        int top = event.resolution.getScaledHeight() - GuiIngameForge.right_height;
-        int currentMana = props.getCurrentMana();
-        int maxMana = props.getMaxMana() / 2;        
-        int regen = -1;
-        
-        if (mc.thePlayer.isPotionActive(ModPotions.manaRegen))
-        	regen = mc.ingameGUI.getUpdateCounter() % 25;
-        
-        for (int i = 0; i < maxMana; ++i)
-        {
-            int idx = i * 2 + 1;
-            int x = left - i * 8 - 9;
-            int y = top;
-            
-        	if (i == regen)
-        		y -= 2;
+        mc.mcProfiler.startSection("air");
+        GL11.glEnable(GL11.GL_BLEND);
+        int left = event.resolution.getScaledWidth() / 2 + 91;
+        int top = event.resolution.getScaledHeight() - GuiIngameForge.right_height - 10;
 
-            this.drawTexturedModalRect(x, y, 0, 0, 9, 9);
-            
-            if (idx < currentMana)
-            	this.drawTexturedModalRect(x, y, 36, 0, 9, 9);
-            else if (idx == currentMana)
-            	this.drawTexturedModalRect(x, y, 45, 0, 9, 9);
+        if (mc.thePlayer.isInsideOfMaterial(Material.water))
+        {
+            int air = mc.thePlayer.getAir();
+            int full = MathHelper.ceiling_double_int((double)(air - 2) * 10.0D / 300.0D);
+            int partial = MathHelper.ceiling_double_int((double)air * 10.0D / 300.0D) - full;
+
+            for (int i = 0; i < full + partial; ++i)
+            {
+                drawTexturedModalRect(left - i * 8 - 9, top, (i < full ? 16 : 25), 18, 9, 9);
+            }
         }
-        
+
         GL11.glDisable(GL11.GL_BLEND);
         mc.mcProfiler.endSection();
+		
+        // Mana bar
+        
+		ManaProperties props = ManaProperties.get(this.mc.thePlayer);
+		
+		if (props != null && props.getMaxMana() != 0)
+		{		
+			mc.mcProfiler.startSection("mana");
+			mc.getTextureManager().bindTexture(modIcons);
+			GL11.glEnable(GL11.GL_BLEND);
+			left = event.resolution.getScaledWidth() / 2 + 91;
+	        top = event.resolution.getScaledHeight() - GuiIngameForge.right_height;
+	        int currentMana = props.getCurrentMana();
+	        int maxMana = props.getMaxMana() / 2;        
+	        int regen = -1;
+	        
+	        if (mc.thePlayer.isPotionActive(ModPotions.manaRegen))
+	        	regen = mc.ingameGUI.getUpdateCounter() % 25;
+	        
+	        for (int i = 0; i < maxMana; ++i)
+	        {
+	            int idx = i * 2 + 1;
+	            int x = left - i * 8 - 9;
+	            int y = top;
+	            
+	        	if (i == regen)
+	        		y -= 2;
+	
+	            this.drawTexturedModalRect(x, y, 0, 0, 9, 9);
+	            
+	            if (idx < currentMana)
+	            	this.drawTexturedModalRect(x, y, 36, 0, 9, 9);
+	            else if (idx == currentMana)
+	            	this.drawTexturedModalRect(x, y, 45, 0, 9, 9);
+	        }
+	        
+	        GL11.glDisable(GL11.GL_BLEND);
+	        mc.mcProfiler.endSection();
+		}
+	}
+	
+	@SubscribeEvent(priority = EventPriority.NORMAL)
+	public void onRenderAirBar(RenderGameOverlayEvent.Pre event)
+	{
+		if (event.type != ElementType.AIR)
+			return;
+		
+		event.setCanceled(true);
 	}
 }
