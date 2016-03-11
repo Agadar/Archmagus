eee738ad118a37493e7b32ae93ee13e7dc78afc3
Agadar <martin_donkersloot@hotmail.com>
2016-03-11 23:49:37 +0100

    'Open spell gui' key can now also close it.

--- java/com/agadar/archmagus/gui/ModGuiHandler.java
+++ java/com/agadar/archmagus/gui/ModGuiHandler.java
@@ -1,12 +1,11 @@
 package com.agadar.archmagus.gui;
 
-import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.ContainerChest;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 
-/* This mod's handler for all GUI's besides the Mana bar. */
+/** This mod's handler for all GUI's besides the Mana bar. */
 public class ModGuiHandler implements IGuiHandler 
 {
 	public static final int SPELLBOOK_GUI = 0;
@@ -21,7 +20,7 @@ public class ModGuiHandler implements IGuiHandler
     @Override
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) 
     {
-    	if (ID == SPELLBOOK_GUI) return new GuiChest(player.inventory, new InventorySpells(player));   		
+    	if (ID == SPELLBOOK_GUI) return new GuiSpells(player.inventory, new InventorySpells(player));   		
     	return null;
     }
 }
